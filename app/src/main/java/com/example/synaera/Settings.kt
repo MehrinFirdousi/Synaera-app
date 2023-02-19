d88fc8b199a0fe851a6257350a90e8eca5b19a4c
AlbusTheDev <qusaih42@gmail.com>
2023-02-19 12:27:21 +0400

    Added Settings skeleton

---
+++ app/src/main/java/com/example/synaera/Settings.kt
@@ -0,0 +1,13 @@
+package com.example.synaera
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Settings : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+
+    }
+}
