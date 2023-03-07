41f0157241d38af1e5207781511c88869e0522f8
AlbusTheDev <qusaih42@gmail.com>
2023-03-07 07:07:01 +0400

    Fixed login, added send button (functional but visually incomplete)

--- app/src/main/java/com/example/synaera/LoginActivity.kt
+++ app/src/main/java/com/example/synaera/LoginActivity.kt
@@ -7,7 +7,7 @@ import com.example.synaera.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var users : ArrayList<User>
+    lateinit var users : ArrayList<User>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityLoginBinding.inflate(layoutInflater)
@@ -15,23 +15,26 @@ class LoginActivity : AppCompatActivity() {
 
         users = ArrayList()
 
-        users.add(User("Qusai", "qusai@gmail.com", "1234567"))
+        users.add(User("Qusai", "qusai@gmail.com", "123"))
 
         binding.signupBttn.setOnClickListener{
             val intent = Intent(this, RegisterActivity::class.java)
-            intent.putParcelableArrayListExtra("Users", users)
             startActivity(intent)
         }
 
         binding.loginBttn.setOnClickListener{
             val email = binding.emailEditText.text.toString()
             val password = binding.passwordEditText.text.toString()
-            if (email.equals("synaera@semaphore.com", true) &&
-                password == "123456") {
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-                finish()
+
+            for (user in users) {
+                if (email.equals(user.email, true) && password == user.password) {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
             }
         }
     }
+
+
 }
--- app/src/main/java/com/example/synaera/RegisterActivity.kt
+++ app/src/main/java/com/example/synaera/RegisterActivity.kt
@@ -5,12 +5,15 @@ import android.os.Bundle
 import com.example.synaera.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    var users = ArrayList<User>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        intent.getParcelableArrayExtra("")
+
     }
 }
